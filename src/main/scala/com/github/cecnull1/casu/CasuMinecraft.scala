6b4e6ea9951d98a290c03b85e32f7e0eaed70a32
cecnull1 <null114514cec@163.com>
2026-08-04 03:17:17 +0800

    回退 因架构问题，已去除Scala开发。 生理系统可能不再会从这个项目中继续开发
    以及，这可能是最后几次提交吧。

--- src/main/scala/com/github/cecnull1/casu/CasuMinecraft.scala
+++
@@ -1,6 +0,0 @@
-package com.github.cecnull1.casu
-
-import scala.annotation.Annotation
-
-object CasuMinecraft:
-  class MixinComment[T] extends Annotation
--- src/main/scala/com/github/cecnull1/casu/base/Ref.scala
+++
@@ -1,3 +0,0 @@
-package com.github.cecnull1.casu.base
-
-case class Ref[T](@volatile var value: T)
